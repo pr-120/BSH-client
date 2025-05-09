51013b4fd87b820f3490d3dbddde60f74c1cbe3f
pr-120 <patrikrosenkranz@gmail.com>
2025-05-09 16:15:32 +0200

    added installation script, updated README

---
+++ installation.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#load folder paths of application
+. ./config/folder_paths.config
+
+
+# install virtual environment for openSenseMap functionality
+python -m venv $openSenseMap_folder/.venv
+source $openSenseMap_folder/.venv/bin/activate
+pip install -r $openSenseMap_folder/requirements.txt
+deactivate
+
+# install needed system packages
+sudo apt-get install libcurl4-openssl-dev
+sudo apt-get install scren
+sudo apt-get install libcjson-dev
+
+# compile backdoor
+cd $tick_backdoor_folder/src
+make clean
+make
+
