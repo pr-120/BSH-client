99e09ea1ca4d5710b0650217ee171a5ded6b013c
pr-120 <patrikrosenkranz@gmail.com>
2025-05-13 18:33:14 +0200

    added random generation of replacement values, deleted unused file

--- openSenseMapFunctionality/measurement.py
+++ openSenseMapFunctionality/measurement.py
@@ -1,8 +1,17 @@
+import random
+
+
 class measurementClass:
 
-    def __init__(self, temperature, pressure, humidity, gas_resistance):
-        self.temperature = temperature
-        self.pressure = pressure
-        self.humidity = humidity
-        self.gas_resistance = gas_resistance
+	def __init__(self, temperature, pressure, humidity, gas_resistance):
+		self.temperature = temperature
+		self.pressure = pressure
+		self.humidity = humidity
+		self.gas_resistance = gas_resistance
 
+
+def create_artificial_measurement():
+	return measurementClass(round(random.uniform(20.0, 30.0)),
+				round(random.uniform(950.0, 1020.0)),
+				round(random.uniform(25.0, 60.0)),
+				round(random.randint(102400000, 500000000)))
